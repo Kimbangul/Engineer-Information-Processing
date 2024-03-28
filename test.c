02bdf70b67053d52f65e85d8534dfb1d3090669a
Kimbangul <highcolor_12@naver.com>
2024-03-28 10:31:44 +0900

    docs: edit docs

--- test.c
+++ test.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-  int arr[2][3] = {1,2,3,4,5,6};
-  int (*p)[3] = NULL; 
+  int arr[2][3] = {1,2,3,4,5,6}; // 
+  int (*p)[3] = NULL;  // p = {1,2,3,4,5,6}
   p = arr;
 
-  printf("%d, ", *(p[0]+1) + *(p[1]+2)); 
-  printf("%d", *(*(p+1)+0) + *(*(p+1)+1));
+  printf("%d, ", *(p[0]+1) + *(p[1]+2)); // 2 + 6 = 5
+  printf("%d", *(*(p+1)+0) + *(*(p+1)+1)); // 4 + 5 = 9
   return 0;
 }
