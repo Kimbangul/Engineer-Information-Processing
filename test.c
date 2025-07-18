d31379ca5ae0d47416347399c01ecc91bea72de5
kimbangul <highcolor_12@naver.com>
2025-07-18 16:37:46 +0900

    250718

--- test.c
+++ test.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char *argv[]){
-  int arr[2][3] = {1,2,3,4,5,6}; // 
-  int (*p)[3] = NULL;  // p = {1,2,3,4,5,6}
-  p = arr;
-
-  printf("%d, ", *(p[0]+1) + *(p[1]+2)); // 2 + 6 = 5
-  printf("%d", *(*(p+1)+0) + *(*(p+1)+1)); // 4 + 5 = 9
+typedef struct
+{
+  char name[10];
+  int score[3];
+} Node;
+int f(char score)
+{
+  return score & 0xA5; -> 165
+}
+int main()
+{
+  Node *nodes = (Node *)malloc(2 * sizeof(Node));
+  nodes[0] = (Node){"Kim", {0xF0, 0xF5, 0xDB}};
+  nodes[1] = (Node){"Lee", {0xED, 0xD3, 0xF2}};
+  int result = 0;
+  for (int i = 0; i < 2; ++i)
+  {
+    result += f(nodes[i].score[0]);
+    result += f(nodes[i].score[1]);
+    result += f(nodes[i].score[2]);
+  }
+  printf("%d\n", result);
+  free(nodes);
   return 0;
 }
+
+char *p = "KOREA"
+printf("%s\n", p); -> "KOREA"
+printf("%s\n", p + 3); -> "EA"
+printf("%s\n", *p); -> "K"
+printf("%s\n", *(p + 3)); -> "E"
+
